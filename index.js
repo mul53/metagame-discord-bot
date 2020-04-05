79107998f7fecb6156682ef0208e9fffee82df60
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-05 16:03:34 +0200

    discord set up

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const DiscordClient = require('./lib/discordClient');
+
+const client = new DiscordClient(process.env.BOT_TOKEN); 
+
+client.onReady(() => {
+    console.log(`Logged in ${client.client.user.username}`);
+});
