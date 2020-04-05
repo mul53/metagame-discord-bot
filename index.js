5ead98ab896fde9459857507a6bcd763e6cbd9e3
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-05 21:17:36 +0200

    refactored code

--- index.js
+++ index.js
@@ -1,69 +1,65 @@
-require('dotenv').config();
-const DiscordClient = require('./lib/discordClient');
+require("dotenv").config();
+const DiscordClient = require("./lib/discordClient");
 
-const client = new DiscordClient(process.env.BOT_TOKEN); 
+const client = new DiscordClient(process.env.BOT_TOKEN);
 
 client.onReady(() => {
-    console.log(`Logged in ${client.client.user.username}`);
+  console.log(`Logged in ${client.client.user.username}`);
 });
 
-client.addChannelListener('quest-completions', async (msg) => {
-    try {
-        const { content, author } = msg;
-        
-        if (content === '!complete quest1') {
-    
-        } else {
-            // delete message from user
-            msg.delete();
-            
-            // dm user telling them the format is wrong
-            let dmChannel = author.dmChannel;
-    
-            if (!dmChannel) dmChannel = await author.createDM();
+client.addChannelListener("quest-completions", async (msg) => {
+  try {
+    const { content, author } = msg;
 
-            dmChannel.send('You sent the wrong format `' + content + '`, to complete a quest send `!complete quest1`');
-        }
-    } catch (e) {
-        console.log(`Error: ${e.message}`);
+    if (content !== "!complete quest1") {
+      // delete message from user
+      msg.delete();
+
+      // dm user telling them the format is wrong
+      client.sendDirectMessage(
+        author,
+        "You sent the wrong format `" +
+          content +
+          "`, to complete a quest send `!complete quest1`"
+      );
     }
+  } catch (e) {
+    console.log(`Error: ${e.message}`);
+  }
 });
 
-client.client.on('messageReactionAdd', async (msgReaction, user) => {
-    try {
-        const { 
-            message: { 
-                channel: { name: channelName, guild }, 
-                content: messageContent,
-                author
-            } 
-        } = msgReaction;
-    
-        if (channelName === 'quest-completions'
-            && messageContent == '!complete quest1') {
-    
-            if (guild.available) {
-                const member = await guild.member(user);
-                const memberRoles = member.roles.cache.map(role => role.name);
+client.client.on("messageReactionAdd", async (msgReaction, user) => {
+  try {
+    const {
+      message: {
+        channel: { name: channelName, guild },
+        content: messageContent,
+        author,
+      },
+    } = msgReaction;
 
-                if (memberRoles.includes('Diamond Founder')) {
-                    const metaGamePlayerRole = guild.
-                        roles.cache.find(role => role.name === 'MetaGame Player');
+    if (
+      channelName === "quest-completions" &&
+      messageContent == "!complete quest1"
+    ) {
+      if (guild.available) {
+        const member = await guild.member(user);
+        const memberRoles = member.roles.cache.map((role) => role.name);
 
-                    const messageAuthor = await guild.member(author);
+        if (memberRoles.includes("Diamond Founder")) {
+          const { roleId } = guild.roles.cache.find(
+            (role) => role.name === "MetaGame Player"
+          );
 
-                    await messageAuthor.roles.add([metaGamePlayerRole.id]);
+          const messageAuthor = await guild.member(author);
 
-                    let dmChannel = author.dmChannel;
+          await messageAuthor.roles.add([roleId]);
 
-                    if (!dmChannel) dmChannel = await author.createDM();
-
-                    dmChannel.send('Welcome to MetaGamer');
-                }
-            }
+          client.sendDirectMessage(author, "Welcome to MetaGamer");
         }
-    } catch (e) {
-        console.log(`Error: ${e.message}`);
+      }
     }
-    
-})
+  } catch (e) {
+    console.log(`Error: ${e.message}`);
+  }
+});
