19c6cd7169d8b1f7473a7b3c6baa6656262d16cd
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-05 18:27:09 +0200

    handled bad format

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ client.onReady(() => {
     console.log(`Logged in ${client.client.user.username}`);
 });
 
-client.addChannelListener('quest-completions', msg => {
-    console.log(msg);
+client.addChannelListener('quest-completions', async (msg) => {
+    try {
+        const { content, author } = msg;
+        
+        if (content === '!complete quest1') {
+    
+        } else {
+            // delete message from user
+            msg.delete();
+            
+            // dm user telling them the format is wrong
+            let dmChannel = author.dmChannel;
+    
+            if (!dmChannel) dmChannel = await author.createDM();
+
+            dmChannel.send('You sent the wrong format `' + content + '`, to complete a quest send `!complete quest1`');
+        }
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+    }
 });
